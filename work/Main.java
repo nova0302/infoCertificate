7b539a1f312efffcd0571427e927fac6dee989ba
Sanglae Kim <nova0302@hotmail.com>
2021-03-29 14:26:08 +0900

    java example for import and package creation added

---
+++ work/Main.java
@@ -0,0 +1,9 @@
+import p1.MyClass;
+
+public class Main{
+    public static void main(String[] args){
+	MyClass myClass = new MyClass();
+	myClass.m1();
+	System.out.println("This is my package");
+    }
+}
---
+++ work/MyClass.java
@@ -0,0 +1,8 @@
+package p1;
+public class MyClass{
+    public MyClass(){}
+    public void m1(){
+	System.out.println("MyClass in p1");
+	System.out.println("MyClass in p1");
+    }
+}
